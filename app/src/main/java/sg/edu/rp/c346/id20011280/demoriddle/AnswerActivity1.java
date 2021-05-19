cc6985b6db6117493815be61bbe37f6f89923ad1
TZane25 <20011280@myrp.edu.sg>
2021-05-19 11:45:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011280/demoriddle/AnswerActivity1.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.id20011280.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity1 extends AppCompatActivity {
+
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer1);
+
+        tvAnswer = findViewById(R.id.textView);
+        Intent received = getIntent();
+        String QuestionSelected = received.getStringExtra("Question");
+        tvAnswer.setText(QuestionSelected + " answer is : Queue");
+
+        
+
+
+
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id20011280/demoriddle/AnswerActivity2.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id20011280.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity2 extends AppCompatActivity {
+
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer2);
+
+        tvAnswer = findViewById(R.id.textView2);
+        Intent received2 = getIntent();
+        String QuestionSelected2 = received2.getStringExtra("Question2");
+        tvAnswer.setText(QuestionSelected2 + " answer is : Gone");
+
+    }
+}
