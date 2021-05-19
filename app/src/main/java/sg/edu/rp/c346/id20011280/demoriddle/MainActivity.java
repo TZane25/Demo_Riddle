2bbc2a35585a2c85095df98a0d7b68ab89a63bfa
TZane25 <83322334+TZane25@users.noreply.github.com>
2021-05-19 18:16:54 +0800

    Update MainActivity.java

--- app/src/main/java/sg/edu/rp/c346/id20011280/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20011280/demoriddle/MainActivity.java
@@ -14,6 +14,37 @@ public class MainActivity extends AppCompatActivity {
     Button btnRevealQ1;
     TextView tvQ2;
     Button btnRevealQ2;
+    
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
